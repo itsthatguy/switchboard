948364076cdfae71a3fed2e06876c19b4da12994
Kevin Altman <kevin@itsthatguy.com>
2015-07-10 12:22:00 -0400

    Adds a really crude adapter select input

--- src/app/chat/sidebar/sidebar.jsx
+++ src/app/chat/sidebar/sidebar.jsx
@@ -1,7 +1,10 @@
 /* global React */
 import Reflux         from 'reflux';
-import SidebarActions from './sidebar.actions';
+import Modal          from 'react-modal';
 import SidebarStore   from './sidebar.store';
+
+Modal.setAppElement(document.getElementById('content'));
+Modal.injectCSS();
 
 export default React.createClass({
   displayName: 'MessagesSidebar',
@@ -11,11 +14,21 @@ export default React.createClass({
   ],
 
   getInitialState () {
-    return { };
+    return {
+      modalIsOpen: false
+    };
   },
 
-  addService (e) {
-    console.log('e: ', e);
+  openModal () {
+    this.setState({modalIsOpen: true});
+  },
+
+  closeModal () {
+    this.setState({modalIsOpen: false});
+  },
+
+  addService () {
+    this.openModal(true);
   },
 
   render () {
@@ -23,6 +36,17 @@ export default React.createClass({
     return (
       <div className='sidebar__wrapper'>
         <div className='sidebar__add-service' onClick={this.addService}>Add</div>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onRequestClose={this.closeModal}
+        >
+          <h3>Choose an adapter</h3>
+          <select>
+            <option>Mock Adapter</option>
+            <option>IRC Adapter</option>
+          </select>
+          <button>Submit</button>
+        </Modal>
       </div>
     );
   }
