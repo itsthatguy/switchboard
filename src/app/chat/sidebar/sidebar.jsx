e393de6d180c86a606feaf660fd39160aba8f42d
Kevin Altman <kevin@itsthatguy.com>
2015-07-10 12:03:10 -0400

    adds click event on add button in sidebar

--- src/app/chat/sidebar/sidebar.jsx
+++ src/app/chat/sidebar/sidebar.jsx
@@ -1,16 +1,29 @@
 /* global React */
+import Reflux         from 'reflux';
+import SidebarActions from './sidebar.actions';
+import SidebarStore   from './sidebar.store';
+
 export default React.createClass({
   displayName: 'MessagesSidebar',
+
+  mixins: [
+    Reflux.listenTo(SidebarStore, 'onAddService')
+  ],
 
   getInitialState () {
     return { };
   },
 
+  addService (e) {
+    console.log('e: ', e);
+  },
 
   render () {
 
     return (
-      <div className='sidebar__wrapper'></div>
+      <div className='sidebar__wrapper'>
+        <div className='sidebar__add-service' onClick={this.addService}>Add</div>
+      </div>
     );
   }
 });
